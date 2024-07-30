5898d35bd97fdec1ae282b77337f3fb241a492cb
AntonellaJarpasolis <dannaejarpasolis@gmail.com>
2024-07-30 16:02:46 -0400

    se completa crud

--- src3/routes/productoRouter.js
+++ src3/routes/productoRouter.js
@@ -12,12 +12,11 @@ router.get('/',async(req,res) => {
         console.log('error'+err)
     }
 })
-
-router.get('/editar/:id',async(req,res) => {
+router.post('/',async(req,res) => {
     try {
-        const {id} = req.params
-        const producto = await Producto.showProductoById(id)
-        return res.render('productos/editar',{producto})
+        const producto = req.body
+        const respuesta = await Producto.addProducto(producto)
+        return res.status(200).json(respuesta)
     } catch (err) {
         console.log('error'+err)
     }
@@ -31,41 +30,30 @@ router.get('/create',async(req,res) => {
     }
 })
 
-router.post('/',async(req,res) => {
-    try {
-        const producto = req.body
-        const respuesta = await Producto.addProducto(producto)
-        return res.status(200).json(respuesta)
-    } catch (err) {
-        console.log('error'+err)
-    }
-})
-
 router.put('/:id',async(req,res) => {
     try {
-        const producto = req.body
         const {id} = req.params
-        const respuesta = await Producto.updateProductoById(id,producto)
+        const producto = req.body
+        const respuesta = await Producto.updateProducto(id,producto)
         return res.status(200).json(respuesta)
     } catch (err) {
         console.log('error'+err)
     }
 })
 
-router.delete('/',async(req,res) => {
+router.get('/:show',async(req,res) => {
     try {
         const {id} = req.query
-        const respuesta = await Producto.deleteProductoById(id)
+        const respuesta = await Producto.showProducto(id)
         return res.status(200).json(respuesta)
     } catch (err) {
         console.log('error'+err)
     }
 })
-
-router.get('/:id',async(req,res) => {
+router.delete('/:id',async(req,res) => {
     try {
         const {id} = req.params
-        const respuesta = await Producto.showProductoById(id)
+        const respuesta = await Producto.deleteProducto(id)
         return res.status(200).json(respuesta)
     } catch (err) {
         console.log('error'+err)
--- src3/services/productoService.js
+++ src3/services/productoService.js
@@ -8,22 +8,19 @@ export default class ProductService{
     getProductos = async() =>{
         const productos = await this.knex.from('productos').select('*')
         return Object.values(JSON.parse(JSON.stringify(productos)))
-    }
-
+    } 
     addProducto = async(producto) => {
         return this.knex('productos').insert(producto)
+        
     }
 
-    updateProductoById = async(id,producto) => {
-        return this.knex('productos').where({producto_id:id}).update(producto);
+    updateProducto = async (id, producto) => {
+        return this.knex('productos').where({producto_id:id}, '=', id).update(producto)
     }
-
-    deleteProductoById = async(id) => {
-        return this.knex('productos').where({producto_id:id}).del();
+    showProducto = async(id) =>{
+        return this.knex('productos').where({producto_id:id}).first()
     }
-
-    showProductoById = async(id) => {
-        return await this.knex('productos').where({producto_id:id}).select('*').first();
+    deleteProducto= async(id) =>{
+        return this.knex('productos').where({producto_id:id}).del()
     }
-
 }
